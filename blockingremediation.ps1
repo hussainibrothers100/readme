9b31b5b434574e765f58e7271603d8d7c75c9b46
hussainibrothers100 <hussainibrothers100@gmail.com>
2024-11-03 01:32:52 +0530

    Update blockingremediation.ps1

--- blockingremediation.ps1
+++ blockingremediation.ps1
@@ -1,14 +1,20 @@
-# Variables
-$serverInstance = "OMWEKNYOSCRPDB2\MSSQLSERVER"  # Replace with your SQL Server instance name
+# set server instance along with ip address and port number
+
+$serverName = "localhost"
+$port = "1435"
+$instanceName = "MSSQLSERVER"
+$serverInstance = "$serverName,$port\$instanceName"
 $threshold = 300000  # Blocking threshold in milliseconds (5 minutes)
 $sqlScript = @"
-    -- Check for blocking processes exceeding the threshold
     IF EXISTS (SELECT 1 
-               FROM master.dbo.sysprocesses S2 WITH (NOLOCK) 
-               WHERE S2.spid != 0 AND S2.blocked != 0 AND S2.blocked != S2.spid AND waittime > $threshold)
+               FROM master.dbo.sysprocesses S2-- WITH (NOLOCK) 
+               --WHERE S2.spid != 0 
+			   --AND S2.blocked != 0 AND S2.blocked != S2.spid
+			   --AND waittime > @threshold
+			   )
     BEGIN
         -- Create a temporary table to store blocking information
-        CREATE TABLE #BlockingInfo (
+        CREATE TABLE BlockingInfo (
             SPID INT,
             BlockingSPID INT,
             SQL_QUERY VARCHAR(MAX),
@@ -20,8 +26,9 @@ $sqlScript = @"
         );
 
         -- Gather information about blocking processes
-        INSERT INTO #BlockingInfo (SPID, BlockingSPID, SQL_QUERY, HostName, LoginName, DatabaseName, ObjectName)
+        INSERT INTO BlockingInfo (SPID, BlockingSPID, SQL_QUERY, HostName, LoginName, DatabaseName, ObjectName)
         SELECT 
+        top 2
             S1.spid,
             S1.blocked,
             sqltxt.text,
@@ -32,46 +39,45 @@ $sqlScript = @"
         FROM master.dbo.sysprocesses S1 WITH (NOLOCK)
         LEFT JOIN sys.dm_tran_locks L1 WITH (NOLOCK) ON S1.spid = L1.request_session_id AND L1.resource_type = 'OBJECT'
         OUTER APPLY sys.dm_exec_sql_text(sql_handle) sqltxt
-        WHERE S1.blocked != 0 AND S1.blocked != S1.spid AND S1.waittime > $threshold;
-"@
-Invoke-Sqlcmd -ServerInstance $serverInstance -Query $sqlScript
-
-$sqlScript = @"
-        -- Kill the blocked sessions
-        DECLARE @killCmd VARCHAR(MAX) = '';
-        SELECT @killCmd = @killCmd + 'KILL ' + CAST(SPID AS VARCHAR(10)) + ';' FROM #BlockingInfo;
-        EXEC(@killCmd);
-
-        -- Construct the email body
-        DECLARE @tableHTML NVARCHAR(MAX);
-        SET @tableHTML =
-            N'<H1>Blocking Alert</H1>' +
-            N'<table border="1">' +
-            N'<tr><th>SPID</th><th>Blocking SPID</th><th>SQL Query</th><th>Hostname</th><th>Login</th><th>Database</th><th>Object</th></tr>' +
-            CAST((SELECT 
-                      td = SPID, '',
-                      td = BlockingSPID, '',
-                      td = SQL_QUERY, '',
-                      td = HostName, '',
-                      td = LoginName, '',
-                      td = DatabaseName, '',
-                      td = ObjectName
-                  FROM #BlockingInfo
-                  FOR XML PATH('tr'), TYPE) AS NVARCHAR(MAX)) +
-            N'</table>';
-
-        -- Send the email notification
-        EXEC msdb.dbo.sp_send_dbmail
-            @profile_name = 'DBA',  -- Replace with your actual DB Mail profile name
-            @recipients = 'youremail@yourdomain.com',  -- Replace with your email address
-            @subject = 'Blocking Processes Killed',
-            @body = @tableHTML,
-            @body_format = 'HTML';
-
-        -- Drop the temporary table
-        DROP TABLE #BlockingInfo;
+        --WHERE S1.blocked != 0 AND S1.blocked != S1.spid AND S1.waittime > @threshold;
+        select * FROM BlockingInfo
+    END
+    ELSE
+    BEGIN
+        PRINT 'No blocking processes found.'
     END
 "@
+# Execute the SQL query using invoke-sqlcmd and store the result in $result
+$result = Invoke-Sqlcmd -ServerInstance $serverInstance -Query $sqlScript
 
-# Execute the SQL script using Invoke-Sqlcmd
-Invoke-Sqlcmd -ServerInstance $serverInstance -Query $sqlScript
+# Loop through each row in the result set
+foreach ($row in $result) {
+    # Access individual columns by their name
+    Write-Host "Current row values:"
+    Write-Host "Column1: $($row.SPID)"
+    Write-Host "Column2: $($row.SQL_QUERY)"
+    # ... and so on
+
+    # Take user input using Read-Host
+    $userInput = Read-Host "Please enter your input for this row"
+
+    # If user selects to kill the process, update the ToBeKilled flag in the temporary table
+    if ($userInput -eq "kill") {
+        $updateQuery = "UPDATE BlockingInfo SET ToBeKilled = 1 WHERE SPID = '$($row.SPID)'"
+        Invoke-Sqlcmd -ServerInstance $serverInstance -Query $updateQuery
+    }
+}
+
+# After processing all rows, execute the final update query to kill the selected processes
+$finalUpdateQuery = "SELECT * FROM BlockingInfo WHERE ToBeKilled = 1"
+
+# After processing all rows, execute the final update query to kill the selected processes
+$finalUpdateQuery = @"
+    DECLARE @killCommand NVARCHAR(MAX) = '';
+    SELECT @killCommand = @killCommand + 'KILL ' + CAST(SPID AS NVARCHAR(10)) + '; '
+    FROM BlockingInfo WHERE ToBeKilled = 1;
+    --EXEC sp_executesql @killCommand;
+    --DROP TABLE BlockingInfo;
+"@
+
+Invoke-Sqlcmd -ServerInstance $serverInstance -Query $finalUpdateQuery
